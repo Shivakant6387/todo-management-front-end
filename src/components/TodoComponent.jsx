a56b571ea62d5c3328814383bd25b7e37a734210
Shivakant6387 <shivakantsingh900@gmail.com>
2024-02-22 16:36:54 +0530

    add todo form

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { useState } from 'react'
 
 const TodoComponent = () => {
+    const [title,setTitle]=useState('');
+    const [description,setDescription]=useState('');
+    const [completed,setCompleted]=useState('');
+    function saveTodo(e){
+        e.preventDefault()
+        const todo={title,description,completed}
+        console.log(todo);
+    }
   return (
-    <div>TodoComponent</div>
+    <div className='container'>
+        <br></br>
+        <br></br>
+        <div className='row'>
+            <div className='card col-md-6 offset-md-3 offset-md-3'>
+                <h2 className='text-center'>Add Todo</h2>
+                <div className='card-body'>
+                    <form>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Todo Title:</label>
+                            <input type='text'
+                            placeholder='Enter Todo Title'
+                            name='title'
+                            className='form-control'
+                            value={title}
+                            onChange={(e)=>setTitle(e.target.value)}></input>
+                        </div>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Todo Description:</label>
+                            <input type='text'
+                            className='form-control'
+                            placeholder='Enter Todo Description'
+                            name='descriptiontle'
+                            value={description}
+                            onChange={(e)=>setDescription(e.target.value)}></input>
+                        </div>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Todo Completed:</label>
+                            <select className='form-control'
+                            name='completed'
+                            value={completed}
+                            onChange={(e)=>setCompleted(e.target.value)}>
+                                <option value="false">No</option>
+                                <option value="true">YES</option>
+                            </select>
+                        </div>
+                        <button className='btn btn-success' onClick={(e)=>saveTodo(e)}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
